952e33ec9bbb27e73429017da575f8a2d70510df
Quunii <kennethmiuraubw@gmail.com>
2018-12-05 19:10:51 -0500

    removed local control board

--- src/main/java/com/team1389/operation/TeleopMain.java
+++ src/main/java/com/team1389/operation/TeleopMain.java
@@ -1,7 +1,7 @@
 package com.team1389.operation;
 
 import com.team1389.robot.RobotSoftware;
-import com.team1389.robot.controls.ControlBoard;
+import com.team1389.hardware.controls.ControlBoard;
 import com.team1389.system.SystemManager;
 
 public class TeleopMain {
